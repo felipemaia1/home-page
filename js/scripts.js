1e80143d1934560b7d0c342fa4498d1c39feb6de
felipemaia1 <lipemaia01@gmail.com>
2018-09-19 12:20:38 -0300

    ajustando js

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,20 @@
-var $ = document.querySelector.bind(document);
-		var menu = $('.hamburger');
-		var containerMenu = $('.container-menu');
-		var menuList = $('.container-menu .list ul');
+var menu = $('.hamburger');
+var containerMenu = $('.container-menu');
+var menuList = $('.container-menu .list ul');
 
-		menu.onclick = function() {
-			containerMenu.style.display = 'block';
-			menuList.style.display = 'block';
-		}
+menu.onclick = function() {
+	containerMenu.style.display = 'block';
+	menuList.style.display = 'block';
+}
 
-		$('nav a').on('click', function() {
-			var href = $(this).attr('href');
-			scroll(event, href);
-		})
+$('nav a').on('click', function() {
+	var href = $(this).attr('href');
+	scroll(event, href);
+})
 
-		function scroll(event, id) {
-			event.preventDefault();
-			$('html, body').animate({
-				scrollTop: $(id).offset().top -35
-			}, 1000);
-		}
+function scroll(event, id) {
+	event.preventDefault();
+	$('html, body').animate({
+		scrollTop: $(id).offset().top -35
+	}, 1000);
+}
